ff4b8e19b95d709e931d71f0b084f8c85e3d0e71
Laura Dean <lldean18@gmail.com>
2026-09-18 09:32:25 +0100

    removing line to get create pypolca dir because it fails if the output dir already exists

--- 03_polish_assembly_with_pypolca.sh
+++ 03_polish_assembly_with_pypolca.sh
@@ -19,15 +19,14 @@ source $HOME/.bash_profile
 #conda create -n pypolca_env pypolca
 conda activate pypolca_env
 
-mkdir -p /gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly/pypolca
-cd /gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly/pypolca
+cd /gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly
 assembly=/gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly/polypolish/323630L_Photorhabduskhanii_polished.fna
 reads1=/gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly/Photorhabdus_short_reads/PKWT_EKDN250027778-1A_22W3MHLT4_L8_1.fq.gz
 reads2=/gpfs01/home/mbzlld/data/bryant/photorhabdus_assembly/Photorhabdus_short_reads/PKWT_EKDN250027778-1A_22W3MHLT4_L8_2.fq.gz
 
 
 # polish the assembly
-pypolca run -a $assembly -1 $reads1 -2 $reads2 -t 8 --careful
+pypolca run -a $assembly -1 $reads1 -2 $reads2 -t 8 -o pypolca --careful
 
 
 # cleanup env
